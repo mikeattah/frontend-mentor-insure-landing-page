a60580a9352dfca6f93d9858d8764b7c5121ad37
Mike Attah <mailenemona@gmail.com>
2021-12-13 12:59:09 +0100

    updated readme

--- README.md
+++ README.md
@@ -28,7 +28,7 @@ Users should be able to:
 
 ### Links
 
-- Solution URL: [Frontend Mentor](https://your-solution-url.com)
+- Solution URL: [Frontend Mentor](https://www.frontendmentor.io/solutions/semantic-html-css-flexbox-javascript-SAp_cNYtX)
 - Live Site URL: [GitHub Pages](https://mikeattah.github.io/insure-landing-page/)
 
 ## My process
--- index.js
+++ index.js
@@ -2,11 +2,13 @@ function mobileNavBar() {
   const mobileNavBar = document.getElementById("mobile-nav-bar");
   const mobileNavIcon = document.getElementById("mobile-nav-icon");
 
-  mobileNavIcon.textContent = "\u2630" ? "X" : "\u2630";
+  // mobileNavIcon.textContent = "☰" ? "X" : "☰"; //  "\u2630" ? "X" : "\u2630";
 
   if (mobileNavBar.className === "nav-bar") {
     mobileNavBar.className += " responsive";
+    mobileNavIcon.textContent = "X";
   } else {
     mobileNavBar.className = "nav-bar";
+    mobileNavIcon.textContent = "☰";
   }
 }
